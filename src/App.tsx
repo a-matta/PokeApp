ce3b534b5c2bbd055a2efe607930e0a9dad297d9
Amrita Matta <matta.amrita@gmail.com>
2022-01-17 18:06:41 +0200

    added hooks and fetch API

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 import Navbar from "react-bootstrap/NavBar";
 import Container from "react-bootstrap/NavBar";
+import axios from "axios";
 
 const App = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((resp) => setPokemons(resp.data.results));
+  }, []);
+
   return (
-    <Navbar expand="lg" variant="light" bg="light">
-      <Container>
-        <Navbar.Brand href="#">Navbar</Navbar.Brand>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" variant="dark" bg="dark">
+        <Container>
+          <Navbar.Brand href="#">Poke App</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <div>
+        <pre>{JSON.stringify(pokemons, null, 2)}</pre>
+      </div>
+    </>
   );
 };
 
